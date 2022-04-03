168ce7d7fe8b714e7182cab4f8a0167ce696404a
KaanDoesNothing <kaanyt123@gmail.com>
2022-04-03 17:43:02 +0200

    Allowing the frontend to use a custom backend api

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,8 @@ let apiVersion = "v2";
 
 export let api = window.location.origin === "http://localhost:3000" ? `http://localhost:5555/api/${apiVersion}` : `https://api.fitness.kaanlikescoding.me/api/${apiVersion}`;
 
+let customAPI = localStorage.get("backend");
+
+if(customAPI) api = customAPI;
+
 // export let api = "http://localhost:5555/api";
