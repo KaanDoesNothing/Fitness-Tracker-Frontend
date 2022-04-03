5607a0a53ecd3aeeecb5928a20a131c82b1cb7e9
KaanDoesNothing <kaanyt123@gmail.com>
2022-04-03 17:10:06 +0200

    update to new api endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,5 @@
-export let api = window.location.origin === "http://localhost:3000" ? "http://localhost:5555/api/v1" : "https://api.fitness.kaanlikescoding.me/api/v1";
+let apiVersion = "v3";
+
+export let api = window.location.origin === "http://localhost:3000" ? `http://localhost:5555/api/${apiVersion}` : `https://api.fitness.kaanlikescoding.me/api/${apiVersion}`;
+
+// export let api = "http://localhost:5555/api";
