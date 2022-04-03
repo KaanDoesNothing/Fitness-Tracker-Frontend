11a2446e250a3f1cff39bd4f11a77a5a2c582223
KaanDoesNothing <kaanyt123@gmail.com>
2022-04-03 17:20:33 +0200

    Bumping the version back to v2 since it's still the same api

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-let apiVersion = "v3";
+let apiVersion = "v2";
 
 export let api = window.location.origin === "http://localhost:3000" ? `http://localhost:5555/api/${apiVersion}` : `https://api.fitness.kaanlikescoding.me/api/${apiVersion}`;
 
