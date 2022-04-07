128ad9509e34a8571db8ed5e1d6281b64e367ac8
KaanDoesNothing <kaanyt123@gmail.com>
2022-04-07 15:40:23 +0200

    Updated the nutrition view page

--- src/pages/dashboard/tabs/nutrition/view.vue
+++ src/pages/dashboard/tabs/nutrition/view.vue
@@ -4,54 +4,77 @@
   <div class="p-5 text-center" v-if="product">
     <sui-image class="p-10 rounded-sm" size="large" :src="product.image_url" />
 
-    <h3>{{product.product_name}}</h3>
+    <h3 class="text-white">{{product.product_name}}</h3>
     <h4 class="text-gray-500">{{product.brands}}</h4>
-    <h5 class="text-gray-500">{{product.product_quantity}}{{product.type}}</h5>
+<!--    <h5 class="text-gray-500">{{product.product_quantity}}{{product.type}}</h5>-->
 
     <br>
 
     <label>Nutrition</label>
-    <sui-table celled>
-      <sui-table-header>
-        <sui-table-row>
-          <sui-table-header-cell>Type</sui-table-header-cell>
-          <sui-table-header-cell>Amount</sui-table-header-cell>
-          <sui-table-header-cell>Per</sui-table-header-cell>
-        </sui-table-row>
-      </sui-table-header>
-      <sui-table-body>
-        <sui-table-row>
-          <sui-table-cell>Calories</sui-table-cell>
-          <sui-table-cell>{{ product.nutriments["energy-kcal_100g"] }}</sui-table-cell>
-          <sui-table-cell>100g</sui-table-cell>
-        </sui-table-row>
-        <sui-table-row>
-          <sui-table-cell>Proteins</sui-table-cell>
-          <sui-table-cell>{{ product.nutriments["proteins_100g"] }}g</sui-table-cell>
-          <sui-table-cell>100g</sui-table-cell>
-        </sui-table-row>
-        <sui-table-row>
-          <sui-table-cell>Carbohydrates</sui-table-cell>
-          <sui-table-cell>{{ product.nutriments["carbohydrates_100g"] }}g</sui-table-cell>
-          <sui-table-cell>100g</sui-table-cell>
-        </sui-table-row>
-        <sui-table-row>
-          <sui-table-cell>Fat</sui-table-cell>
-          <sui-table-cell>{{ product.nutriments["fat_100g"] }}g</sui-table-cell>
-          <sui-table-cell>100g</sui-table-cell>
-        </sui-table-row>
-        <sui-table-row>
-          <sui-table-cell>Sugars</sui-table-cell>
-          <sui-table-cell>{{ product.nutriments["sugars_100g"] }}g</sui-table-cell>
-          <sui-table-cell>100g</sui-table-cell>
-        </sui-table-row>
-        <sui-table-row>
-          <sui-table-cell>Salt</sui-table-cell>
-          <sui-table-cell>{{ product.nutriments["salt_100g"] }}g</sui-table-cell>
-          <sui-table-cell>100g</sui-table-cell>
-        </sui-table-row>
-      </sui-table-body>
-    </sui-table>
+
+    <div>
+      <label class="text-white font-sans text-xl">Information per 100g/ml</label>
+
+      <FItem v-slot:content :title="`Calories`">
+        <label class="text-gray-500">{{ product.nutriments["energy-kcal_100g"] }}g</label>
+      </FItem>
+      <FItem v-slot:content :title="`Proteins`">
+        <label class="text-gray-500">{{ product.nutriments["proteins_100g"] }}g</label>
+      </FItem>
+      <FItem v-slot:content :title="`Sugars`">
+        <label class="text-gray-500">{{ product.nutriments["sugars_100g"] }}g</label>
+      </FItem>
+      <FItem v-slot:content :title="`Carbohydrates`">
+        <label class="text-gray-500">{{ product.nutriments["carbohydrates_100g"] }}g</label>
+      </FItem>
+      <FItem v-slot:content :title="`Fat`">
+        <label class="text-gray-500">{{ product.nutriments["fat_100g"] }}g</label>
+      </FItem>
+      <FItem v-slot:content :title="`Salt`">
+        <label class="text-gray-500">{{ product.nutriments["salt_100g"] }}g</label>
+      </FItem>
+    </div>
+<!--    <sui-table celled>-->
+<!--      <sui-table-header>-->
+<!--        <sui-table-row>-->
+<!--          <sui-table-header-cell>Type</sui-table-header-cell>-->
+<!--          <sui-table-header-cell>Amount</sui-table-header-cell>-->
+<!--          <sui-table-header-cell>Per</sui-table-header-cell>-->
+<!--        </sui-table-row>-->
+<!--      </sui-table-header>-->
+<!--      <sui-table-body>-->
+<!--        <sui-table-row>-->
+<!--          <sui-table-cell>Calories</sui-table-cell>-->
+<!--          <sui-table-cell>{{ product.nutriments["energy-kcal_100g"] }}</sui-table-cell>-->
+<!--          <sui-table-cell>100g</sui-table-cell>-->
+<!--        </sui-table-row>-->
+<!--        <sui-table-row>-->
+<!--          <sui-table-cell>Proteins</sui-table-cell>-->
+<!--          <sui-table-cell>{{ product.nutriments["proteins_100g"] }}g</sui-table-cell>-->
+<!--          <sui-table-cell>100g</sui-table-cell>-->
+<!--        </sui-table-row>-->
+<!--        <sui-table-row>-->
+<!--          <sui-table-cell>Carbohydrates</sui-table-cell>-->
+<!--          <sui-table-cell>{{ product.nutriments["carbohydrates_100g"] }}g</sui-table-cell>-->
+<!--          <sui-table-cell>100g</sui-table-cell>-->
+<!--        </sui-table-row>-->
+<!--        <sui-table-row>-->
+<!--          <sui-table-cell>Fat</sui-table-cell>-->
+<!--          <sui-table-cell>{{ product.nutriments["fat_100g"] }}g</sui-table-cell>-->
+<!--          <sui-table-cell>100g</sui-table-cell>-->
+<!--        </sui-table-row>-->
+<!--        <sui-table-row>-->
+<!--          <sui-table-cell>Sugars</sui-table-cell>-->
+<!--          <sui-table-cell>{{ product.nutriments["sugars_100g"] }}g</sui-table-cell>-->
+<!--          <sui-table-cell>100g</sui-table-cell>-->
+<!--        </sui-table-row>-->
+<!--        <sui-table-row>-->
+<!--          <sui-table-cell>Salt</sui-table-cell>-->
+<!--          <sui-table-cell>{{ product.nutriments["salt_100g"] }}g</sui-table-cell>-->
+<!--          <sui-table-cell>100g</sui-table-cell>-->
+<!--        </sui-table-row>-->
+<!--      </sui-table-body>-->
+<!--    </sui-table>-->
 
   </div>
 
@@ -67,10 +90,11 @@ import {useRoute} from "vue-router";
 
 import {api} from "../../../../config"
 import Navigation from "../../navigation.vue";
+import FItem from "../../../../components/ui/FItem.vue";
 
 export default defineComponent({
   name: "view",
-  components: {Navigation},
+  components: {FItem, Navigation},
   setup() {
     const route = useRoute();
 
